e508dd05bf852b0bb7baef6fd42a63861704e6e3
Malyaka-Imran <55777834+Malyaka-Imran@users.noreply.github.com>
2021-07-01 21:00:00 -0400

    starting off

---
+++ Merging.py
@@ -0,0 +1,27 @@
+import glob
+import datetime 
+import pandas as pd
+
+print('Merging...')
+# Collect names of all files in data file with .csv extension into a list
+files = glob.glob('./data_all_race_edit/*.csv', recursive = True)
+
+# Removing './' that are included in the file name
+for n in files:
+    i = files.index(n)
+    files[i] = files[i].replace('./','')
+
+# Combining all files into one data frame
+data = pd.DataFrame()
+for n in files:
+    i = files.index(n)
+    df = pd.read_csv(files[i])
+    data = data.append(df)
+
+# Receiving today's date
+currentDT = datetime.datetime.now()
+date = currentDT.strftime("_%Y_%m_%d")
+
+# Exporting data frame as a .csv with today's date appended. 
+export = data.to_csv(r'./Master_data'+ date +'.csv', index = None, header=True)
+print('Merging complete')
